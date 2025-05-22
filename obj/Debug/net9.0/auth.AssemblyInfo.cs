c489123e124ae8593efa9b442366353506b248bf
Friend0nyl1 <thanakit.work.lab@gmail.com>
2025-05-22 19:22:01 +0700

    change db

--- obj/Debug/net9.0/auth.AssemblyInfo.cs
+++ obj/Debug/net9.0/auth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6c0767ed62f7fb59732996a87b4ebf0ac5a7f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
